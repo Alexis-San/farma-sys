a9ae00d108149437700cca8fb4a5a0f22a68747f
EugenioSantacruz <eugenio17santacruz7@gmail.com>
2024-05-12 14:49:17 -0400

    commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
-import { IonButton, IonCheckbox, IonCol, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { IonButton, IonCheckbox, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import './login.css';
 
 const Login: React.FC = () => {
 const navigation = useIonRouter();
@@ -11,24 +12,36 @@ const doLogin =() => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Iniciar Sesion</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle class="centrado">Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <form className="block">
-      <IonItem>
-        <IonLabel position="floating">Usuario</IonLabel>
-        <IonInput />
-      </IonItem>
-      <IonItem>
-        <IonLabel position="floating">Contrasenha</IonLabel>
-        <IonInput type="password" />
-      </IonItem>
-      </form>
-      <IonContent className='ion-padding'>
-       <IonButton onClick={() => doLogin()} expand='block'>
-        Iniciar
-       </IonButton>
+      
+      <IonContent >
+      <IonGrid color='white' >
+        <IonRow>
+          <IonCol class="prueba" >1</IonCol>
+          <IonCol size="4">
+            <br />
+
+            <br />
+
+            <IonInput label="Usuario" labelPlacement="floating" fill="outline" placeholder="Texto"></IonInput>
+
+            <br />
+
+            <IonInput label="Contraseña" labelPlacement="floating" fill="outline" placeholder="Texto" color="dark"></IonInput>
+
+            <br />
+
+            <IonButton onClick={() => doLogin()} expand='full' color="primary">
+             Ingresar
+            </IonButton>
+          </IonCol>
+          <IonCol class="prueba" >Holaaaaa</IonCol>
+        </IonRow>
+      </IonGrid>
+
       </IonContent>
     </IonPage>
   );
